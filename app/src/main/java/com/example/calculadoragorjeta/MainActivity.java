9b497a7e2f681311f9ab11fa5e27e3f8d48d4517
Bruna Mussi <brunamussif2@gmail.com>
2022-05-24 16:06:56 -0300

    colorSetting&restartButtom

--- app/src/main/java/com/example/calculadoragorjeta/MainActivity.java
+++ app/src/main/java/com/example/calculadoragorjeta/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.calculadoragorjeta;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
@@ -47,29 +48,36 @@ public class MainActivity extends AppCompatActivity {
                 calculoGorjeta();
             }
 
-            public void calculoGorjeta() {
-
-                String valorRecuperado = textConta.getText().toString();
-
-                if (valorRecuperado == null || valorRecuperado.equals("")) {
-                    Toast.makeText(
-                            getApplicationContext(),
-                            "Digite um valor!",
-                            Toast.LENGTH_SHORT).show();
-                } else {
-                    double valorDigitado = Double.parseDouble(valorRecuperado);
-
-                    double gorjeta = valorDigitado * (porcentagem / 100);
-                    //new DecimalFormat(".##").format(gorjeta);//
-
-                    double total = gorjeta + valorDigitado;
-                    //new DecimalFormat(".##").format(total);//
-
-                    textGorjeta.setText("R$" + gorjeta);
-                    textTotal.setText("R$" + total);
-                }
-
-            }
         });
+        }
+
+    public void calculoGorjeta() {
+
+        String valorRecuperado = textConta.getText().toString();
+
+        if (valorRecuperado == null || valorRecuperado.equals("")) {
+            Toast.makeText(
+                    getApplicationContext(),
+                    "Digite um valor!",
+                    Toast.LENGTH_SHORT).show();
+        } else {
+            double valorDigitado = Double.parseDouble(valorRecuperado);
+
+            double gorjeta = valorDigitado * (porcentagem / 100);
+            //new DecimalFormat(".##").format(gorjeta);//
+
+            double total = gorjeta + valorDigitado;
+            //new DecimalFormat(".##").format(total);//
+
+            textGorjeta.setText("R$" + gorjeta);
+            textTotal.setText("R$" + total);
+        }
+    }
+
+    public void restartButtom(View view){
+        seekBar.setProgress(0);
+        textConta.setText(null);
+        textGorjeta.setText("R$ 0.00");
+        textTotal.setText("R$ 0.00");
     }
 }
